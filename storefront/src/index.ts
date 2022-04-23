d6c3ea561ca419000811c0029347236e69aa4cf6
ahmedashouryounes <ahmedashouryounes@gmail.com>
2022-04-23 18:16:11 +0200

    Test eslint and prettier and build

--- storefront/src/index.ts
+++ storefront/src/index.ts
@@ -1,12 +1,10 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
-
 const port = process.env.PORT || 3000;
 
-
-app.listen(port,()=>console.log(`Server is running on port ${port}`))
+app.listen(port, () => console.log(`Server is running on port ${port}`));
